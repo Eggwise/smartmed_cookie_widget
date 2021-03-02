d4d54d48fd328424d11509ddec244e59f8917c10
Eggy <jonathan@onlinegastheer.nl>
2021-03-02 20:57:02 +0100

    update

--- src/services/cookie_widget/index.js
+++ src/services/cookie_widget/index.js
@@ -3,9 +3,11 @@
  */
 
 
+
+
 export default {
 
-    get_language(){
+    get_language() {
 
         let url = window.location.href;
 
@@ -21,7 +23,7 @@ export default {
     },
 
 
-    add_script_tag(src, on_load){
+    add_script_tag(src, on_load) {
         let script_element = document.createElement('script');
         script_element.setAttribute('src', src);
         script_element.setAttribute('crossorigin', '');
@@ -29,76 +31,52 @@ export default {
 
         document.body.appendChild(script_element);
 
+
         script_element.addEventListener('load', function () {
             on_load()
         });
     },
 
-    init(){
+    init() {
 
         let hide = !!document.querySelector('#dont_show_cookie_modal_on_page');
 
         console.log('Hide modal ?', hide);
         if (hide) {
             console.log('Dont show cookie modal on this page');
-
-            let amount = 10;
-
-            //hacky script to hide..
-            const hide_metomic = _ => {
-                console.log('Hide metomic')
-
-                window.Metomic('ConsentManager:hide');
-                amount = amount * 1.3;
-                setTimeout(hide_metomic, amount);
-            };
-
-            hide_metomic();
             return;
+
         }
+
         let lang = this.get_language();
+        const options = {
+            backgroundColor: '#012359',
+            cookieKey: 'cookie-consent'
+        }
+
+        if (lang !== 'nl') {
+            options.language = lang
+        } else {
+            const content = {
+                title: 'Cookiemelding',
+                content: 'Wij gebruiken cookies om uw ervaring op onze website te optimaliseren.',
+                accept: 'Accepteren',
+                learnMore: 'AAKJHGHJKJHGF',
+            }
+
+            options.content = content;
+        }
 
 
-        window.Metomic('configure', {
-            language: lang,
+
+        window.CookieBoxConfig = options;
+
+        const url = 'https://cdn.jsdelivr.net/npm/cookie-consent-box@2.4.0/dist/cookie-consent-box.min.js'
+
+        this.add_script_tag(url, () => {
+            console.log('OMG')
         })
 
-        return
-        //
-        // console.log('Set up for language', lang)
-        //
-        // let language_id_mapping = {
-        //     'NL': 'prj:1511a01a-48af-4566-8d5c-f21bebbc2c96',
-        //     'DE': 'prj:12a1dca9-85f0-4854-9661-b84848d24bd5',
-        //     'EN': 'prj:ffaeeacc-3eaa-4344-a5af-57f1e8611b08',
-        //
-        // };
-        //
-        //
-        //
-        //
-        // let id = language_id_mapping[lang];
-        //
-        //
-        // //
-        // // let config_script_url = `https://config.metomic.io/config.js?id=${id}`;
-        // // let base_script_url = `https://consent-manager.metomic.io/embed.js`;
-        // //
-        // // this.add_script_tag(config_script_url, ()=> {
-        // //     console.log('config script loaded')
-        // // });
-        // //
-        // //
-        // // this.add_script_tag(base_script_url, ()=> {
-        // //     console.log('Base script loaded')
-        // //     window.Metomic('ConsentManager:show')
-        // //     console.log("METOMIC", window.Metomic)
-        // // });
-        // //
-        //
-        //
-        //
-        // console.log('Scripts added to head')
 
     }
 
